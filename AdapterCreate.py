21759732378a78fdb4a75f4b0cde779a75769ad5
Matin <matin.akbari06ap@gmail.com>
2026-07-27 01:33:16 +0330

    add script

--- AdapterCreate.py
+++ AdapterCreate.py
@@ -8,7 +8,7 @@ wintun = ctypes.CDLL(os.path.join(os.getcwd(), "wintun.dll"))
 
 WINTUN_ADAPTER_TYPE_NAME = "Wintun"
 
-def create_adapter(name : str):
+def create_adapter(name : str = "vpncore"):
     
     guid = None
     adapter = wintun.WintunCreateAdapter(name, WINTUN_ADAPTER_TYPE_NAME, guid)
@@ -32,3 +32,10 @@ if __name__ == "__main__":
         
     except Exception as e:
         print(e)
+
+
+
+
+# netsh interface ip set address "vpncore" static 10.0.0.1 255.255.255.0
+# route add 0.0.0.0 mask 0.0.0.0 10.0.0.1 metric 5
+
