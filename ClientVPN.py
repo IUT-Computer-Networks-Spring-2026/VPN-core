9da1c8ec7654036350e1cc80dc7fb420085c2387
Matin <matin.akbari06ap@gmail.com>
2026-07-26 22:12:32 +0330

    Create client class

---
+++ ClientVPN.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+import Validator
+
+class Connection_method(Enum):
+    Proxy = 1
+    Tunnel = 2
+    
+
+
+class ClientVPN :  
+    MAX_PACKET_SIZE = 1500
+
+    def __init__(self, ip, port):
+        Validator.ipaddress(ip,port)
+        self.host = ip
+        self.port = port
+        self.connected = False
+
+    def connect(self):
+        self.connected = True
+        return f"Connected to {self.host}:{self.port}"
+
+    def send(self, data):
+        return f"Sending {len(data)} bytes"
+
+    def __del__(self):
+        pass
