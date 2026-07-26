943ee39be568beb390d2694c6f66069483946071
Matin <matin.akbari06ap@gmail.com>
2026-07-26 23:06:30 +0330

    Adapter Creator

---
+++ AdapterCreate.py
@@ -0,0 +1,34 @@
+import ctypes
+import os
+import sys
+
+
+wintun = ctypes.CDLL(os.path.join(os.getcwd(), "wintun.dll"))
+
+
+WINTUN_ADAPTER_TYPE_NAME = "Wintun"
+
+def create_adapter(name : str):
+    
+    guid = None
+    adapter = wintun.WintunCreateAdapter(name, WINTUN_ADAPTER_TYPE_NAME, guid)
+    
+    if not adapter:
+        raise Exception("Can't create Adapter")
+    
+    print(f"Adapter Created : {name}")
+    return adapter
+
+
+if __name__ == "__main__":
+    try:
+        adapter = create_adapter()
+        
+        input("Enter to delete Adapter")
+        
+        
+        wintun.WintunDeleteAdapter(adapter)
+        print("End")
+        
+    except Exception as e:
+        print(e)
