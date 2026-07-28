01171d9e756a7ef0a6d2b19dbabbcacdc6a87c41
Matin <matin.akbari06ap@gmail.com>
2026-07-29 02:42:53 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,18 @@
-#just for multi-file
+from __future__ import annotations
+
+__all__ = [
+    "VpnCore",
+    "Adapter",
+    "RouteManager",
+    "PacketPipeline",
+    "PacketHandler",
+    "ensure_admin",
+]
+
+try:
+    from main import VpnCore
+    from adapter import Adapter
+    from routing import RouteManager
+    from elevation import ensure_admin
+except ImportError:
+    pass
