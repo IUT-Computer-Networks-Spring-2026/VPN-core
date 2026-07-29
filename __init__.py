a35129c922337b3da5b31215d4b2bf086ff1196a
Matin <matin.akbari06ap@gmail.com>
2026-07-29 21:49:58 +0330

    delete additional

--- __init__.py
+++ __init__.py
@@ -1,16 +1,12 @@
 from __future__ import annotations
 
 __all__ = [
-    "VpnCore",
     "Adapter",
     "RouteManager",
-    "PacketPipeline",
-    "PacketHandler",
     "ensure_admin",
 ]
 
 try:
-    from main import VpnCore
     from adapter import Adapter
     from routing import RouteManager
     from elevation import ensure_admin
